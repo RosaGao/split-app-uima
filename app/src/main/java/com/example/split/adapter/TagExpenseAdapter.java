0fd15144b0080562ac82cd91c829effe6461e195
Rosa <ygao112@jhu.edu>
2023-03-25 22:02:53 -0400

    feat: create adapters for Expense items

---
+++ app/src/main/java/com/example/split/adapter/TagExpenseAdapter.java
@@ -0,0 +1,4 @@
+package com.example.split.adapter;
+
+public class TagExpenseAdapter {
+}
---
+++ app/src/main/java/com/example/split/entity/SplitMethod.java
@@ -0,0 +1,7 @@
+package com.example.split.entity;
+
+public enum SplitMethod {
+    EQUAL,
+    PERCENT,
+    EXACT
+}
