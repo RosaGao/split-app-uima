ef92a054d7c0478df2607863e95356f7c0111d07
Rosa <ygao112@jhu.edu>
2023-04-01 14:18:09 -0400

    feat: renames files

---
+++ app/src/main/java/com/example/split/SelectParticipantsActivity.java
@@ -0,0 +1,23 @@
+package com.example.split;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.example.split.entity.User;
+import android.widget.ArrayAdapter;
+
+
+public class SelectParticipantsActivity extends AppCompatActivity {
+//
+//    public class ParkAdapter extends ArrayAdapter<User> {
+//
+//    }
+
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_expense_participants);
+    }
+}
