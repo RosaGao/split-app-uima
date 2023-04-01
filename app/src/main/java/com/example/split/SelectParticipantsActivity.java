ec4c7ff176fc50b0e851e38bb9c09c2324fb3d85
Rosa <ygao112@jhu.edu>
2023-04-01 15:39:11 -0400

    feat: implement select participants and set payer activities

--- app/src/main/java/com/example/split/SelectParticipantsActivity.java
+++ app/src/main/java/com/example/split/SelectParticipantsActivity.java
@@ -2,22 +2,55 @@ package com.example.split;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import com.example.split.databinding.ActivityMainBinding;
+import com.example.split.databinding.ActivityNewExpenseParticipantsBinding;
 import com.example.split.entity.User;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Set;
+
 
 public class SelectParticipantsActivity extends AppCompatActivity {
-//
-//    public class ParkAdapter extends ArrayAdapter<User> {
-//
-//    }
 
-        @Override
+    private ActivityNewExpenseParticipantsBinding binding;
+    public static ArrayList<User> users = new ArrayList<>();
+    public static UsersAdapter adapter;
+    public static List<User> participants;
+
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_expense_participants);
+
+        adapter = new UsersAdapter(this, R.layout.activity_new_expense_participants, users);
+        participants = new ArrayList<>();
+
+
+        binding = ActivityNewExpenseParticipantsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
