8102ce3709106d6c1939174ca200e4e167c2d84f
Brandon Tang <btang13@jh.edu>
2023-04-03 19:40:40 -0400

    added userID, expense list

--- app/src/main/java/com/example/split/entity/User.java
+++ app/src/main/java/com/example/split/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.split.entity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -10,11 +11,13 @@ public class User {
     private String name;                        // Display Name
     private String email;                       // Email
     private String password;                    // Plaintext for now
-    private int phone;                       // Phone Number
+    private int phone;                          // Phone Number
+    private String userID;
 
     private Map<User, Integer> friends;         // User, amount owed (should be equal magnitude between friends)
-    private Set<User> pending_incoming;            // Incoming friend requests
-    private Set<User> pending_outgoing;            // Outgoing friend requests
+    private Set<User> pending_incoming;         // Incoming friend requests
+    private Set<User> pending_outgoing;         // Outgoing friend requests
+    private List<Expense> expense_list;         // List of expenses
 
     public User(String name, String email, int phone, String password) {
         this.name = name;
@@ -24,6 +27,7 @@ public class User {
         this.friends = new HashMap<User, Integer>();
         this.pending_incoming = new HashSet<User>();
         this.pending_outgoing = new HashSet<User>();
+        this.expense_list = new ArrayList<Expense>();
     }
 
     public String get_name() { return this.name; }
@@ -32,27 +36,35 @@ public class User {
     public void edit_name(String name) { this.name = name; }
     public void edit_email(String email) { this.email = email; }
     public void edit_phone(int phone) { this.phone = phone; }
-    public void add_friend_request (User user) {
+    public void add_friend_request(User user) {
         this.pending_incoming.add(user);
     }
-    public void send_friend_request (User user) {
+    public void send_friend_request(User user) {
         this.pending_outgoing.add(user);
     }
-    public void accept_friend_request (User user) {
+    public void accept_friend_request(User user) {
         if(this.pending_incoming.contains(user)) {
             this.pending_incoming.remove(user);
             this.friends.put(user, 0);
         }
     }
 
-    public void change_owed (User user, int amount) {
+    public void change_owed(User user, int amount) {
         if(this.friends.containsKey(user)) {
             this.friends.put(user, amount);
         }
     }
-    public int get_owed (User user) {
+    public int get_owed(User user) {
         return this.friends.getOrDefault(user, 0);
     }
 
-    // generate unique token id
+    public void add_expense(Expense expense) { expense_list.add(expense); }
+    public void remove_expense (Expense expense) {
+        if (expense_list.contains(expense)) {
+            expense_list.remove(expense);
+        }
+    }
+
+    public void set_id(String userID) { this.userID = userID; }
+    public String get_id() { return this.userID; }
 }
