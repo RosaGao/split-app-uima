29d3a0d13a9852b86d69f50054940e57df37eaf7
kyang117 <kyang11702@gmail.com>
2023-04-06 04:08:11 -0400

    fixed rendering issues

---
+++ app/src/main/java/com/example/split/newExpense/SplitMethodActivity.java
@@ -0,0 +1,16 @@
+package com.example.split.newExpense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.split.R;
+
+public class SplitMethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_split_method);
+    }
+}
