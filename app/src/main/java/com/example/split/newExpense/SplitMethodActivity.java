23a8e4d54de0e6a9298f9720c1da8e5378826bc6
kyang117 <kyang11702@gmail.com>
2023-04-06 13:49:27 -0400

    created different fragments for split method

--- app/src/main/java/com/example/split/newExpense/SplitMethodActivity.java
+++ app/src/main/java/com/example/split/newExpense/SplitMethodActivity.java
@@ -3,6 +3,8 @@ package com.example.split.newExpense;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.split.R;
 
@@ -12,5 +14,16 @@ public class SplitMethodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_split_method);
+
+        Button btnequally = findViewById(R.id.btnEqually);
+        btnequally.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+           }
+        });
+
+        // change content of text view (way of split)
+        // participant list ? with adapters . change content in same way as text view, as long as same list of users
     }
 }
