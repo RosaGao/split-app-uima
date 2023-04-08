ae4d8ba9e9a8f8d6d4d6fe3f6b3a5cf19eca5986
franksun <franksun1027@gmail.com>
2023-04-08 16:15:41 -0400

    select participants now navigate to select payer

--- app/src/main/java/com/example/split/newExpense/SelectParticipantsActivity.java
+++ app/src/main/java/com/example/split/newExpense/SelectParticipantsActivity.java
@@ -2,15 +2,22 @@ package com.example.split.newExpense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.split.NewExpenseActivity;
 import com.example.split.R;
 import com.example.split.databinding.ActivityMainBinding;
 import com.example.split.databinding.ActivityNewExpenseParticipantsBinding;
 import com.example.split.entity.User;
+import com.example.split.ui.home.HomeFragment;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class SelectParticipantsActivity extends AppCompatActivity {
@@ -21,17 +28,32 @@ public class SelectParticipantsActivity extends AppCompatActivity {
     public static List<User> participants = new ArrayList<>();
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_expense_participants);
+        binding = ActivityNewExpenseParticipantsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        androidx.appcompat.widget.Toolbar toolBar = findViewById(R.id.newExpenseToolbar);
+        setSupportActionBar(toolBar);
 
         adapter = new UsersAdapter(this, R.layout.activity_new_expense_participants);
         participants = new ArrayList<>();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.check_image_button, menu);
+        return true;
+    }
 
 
-        binding = ActivityNewExpenseParticipantsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.check_image_button) {
+            Intent goToSelectPayer = new Intent(SelectParticipantsActivity.this, SelectPayerActivity.class);
+            startActivity(goToSelectPayer);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
