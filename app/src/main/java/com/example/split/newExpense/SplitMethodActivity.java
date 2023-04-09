63aa2e954928646a934b6ce1c4a97f694815622c
Rosa <ygao112@jhu.edu>
2023-04-08 23:57:09 -0400

    feat: implement split method activity with computation and error handling logic, need to doublecheck

--- app/src/main/java/com/example/split/newExpense/SplitMethodActivity.java
+++ app/src/main/java/com/example/split/newExpense/SplitMethodActivity.java
@@ -2,21 +2,57 @@ package com.example.split.newExpense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.split.NewExpenseActivity;
 import com.example.split.R;
+
+import com.example.split.databinding.ActivitySplitMethodBinding;
+import com.example.split.entity.SplitMethod;
+import com.example.split.entity.User;
 import com.google.android.material.chip.Chip;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SplitMethodActivity extends AppCompatActivity {
+
+    ActivitySplitMethodBinding binding;
+    SplitMethodParticipantAdapter adapter;
+    public static SplitMethod method = SplitMethod.EQUAL;
+    public static String amount;
+    List<User> participantsToSplit = new ArrayList<>(NewExpenseActivity.finalParticipants);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_split_method);
+
+        binding = ActivitySplitMethodBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        androidx.appcompat.widget.Toolbar toolBar = findViewById(R.id.newExpenseToolbar);
+        setSupportActionBar(binding.newExpenseToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolBar.setNavigationIcon(R.drawable.baseline_arrow_back_24);
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NewExpenseActivity.method = null; // nullify any selection since didn't hit check button
+                finish();
+            }
+        });
 
         Chip chipEqually = findViewById(R.id.equal_split_method_chip);
         Chip chipPercent = findViewById(R.id.percentage_split_method_chip);
@@ -26,9 +62,14 @@ public class SplitMethodActivity extends AppCompatActivity {
 
         int yellow = getApplicationContext().getResources().getColor(R.color.yellow);
         int dark_blue = getApplicationContext().getResources().getColor(R.color.dark_blue);
+
+        amount = getIntent().getStringExtra("amount");
+
         chipEqually.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                method = SplitMethod.EQUAL;
+                adapter.notifyDataSetChanged();
                 chipEqually.setChipBackgroundColor(ColorStateList.valueOf(yellow));
                 chipEqually.setTextColor(Color.BLACK);
                 chipEqually.setChipIconResource(R.drawable.equalsplit_icon);
@@ -41,13 +82,15 @@ public class SplitMethodActivity extends AppCompatActivity {
                 chipExact.setTextColor(Color.WHITE);
                 chipExact.setChipIconResource(R.drawable.exactsplit_icon);
 
-                splittingText.setText("You are splitting by equal number for (total cost) dollars.");
+                splittingText.setText("You are splitting by equal number for " + amount + " dollars.");
             }
         });
 
         chipPercent.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                method = SplitMethod.PERCENT;
+                adapter.notifyDataSetChanged();
                 chipEqually.setChipBackgroundColor(ColorStateList.valueOf(dark_blue));
                 chipEqually.setTextColor(Color.WHITE);
                 chipEqually.setChipIconResource(R.drawable.whiteequalsplit);
@@ -60,13 +103,16 @@ public class SplitMethodActivity extends AppCompatActivity {
                 chipExact.setTextColor(Color.WHITE);
                 chipExact.setChipIconResource(R.drawable.exactsplit_icon);
 
-                splittingText.setText("You are splitting by % for (total cost) dollars.");
+                splittingText.setText("You are splitting by % for " + amount + " dollars.");
             }
         });
 
         chipExact.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                method = SplitMethod.EXACT;
+                adapter.notifyDataSetChanged();
+
                 chipEqually.setChipBackgroundColor(ColorStateList.valueOf(dark_blue));
                 chipEqually.setTextColor(Color.WHITE);
                 chipEqually.setChipIconResource(R.drawable.whiteequalsplit);
@@ -79,23 +125,92 @@ public class SplitMethodActivity extends AppCompatActivity {
                 chipExact.setTextColor(Color.BLACK);
                 chipExact.setChipIconResource(R.drawable.blackexactsplit);
 
-                splittingText.setText("You are splitting by exact number for (total cost) dollars.");
+                splittingText.setText("You are splitting by exact number for " + amount + " dollars.");
             }
         });
 
 
+        participantsToSplit.remove(NewExpenseActivity.finalPayer);
+        adapter = new SplitMethodParticipantAdapter(this, R.layout.activity_split_method_participant_layout, participantsToSplit);
+        binding.userslist.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
 
-
-
-//        Button btnequally = findViewById(R.id.btnEqually);
-//        btnequally.setOnClickListener(new View.OnClickListener() {
-//           @Override
-//           public void onClick(View view) {
-//
-//           }
-//        });
-
-        // change content of text view (way of split)
-        // participant list ? with adapters . change content in same way as text view, as long as same list of users
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.check_image_button, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.check_image_button) {
+            View v;
+
+            List<Double> inputs = new ArrayList<>();
+            double total = 0;
+
+            EditText edittext;
+
+            for (int i = 0; i < participantsToSplit.size(); i++) {
+                User participant = participantsToSplit.get(i);
+
+                v = binding.userslist.getChildAt(i);
+                edittext = (EditText) v.findViewById(R.id.inputtextedit);
+                String input = edittext.getText().toString();
+                if (TextUtils.isEmpty(input)) {
+                    edittext.setError("Required");
+                    return false;
+                }
+                try {
+                    double value = Double.parseDouble(input);
+                    inputs.add(Double.valueOf(input));
+                    total += value;
+                } catch (Exception e) {
+                    Snackbar.make(getWindow().getDecorView().getRootView()
+                                    , "Invalid input format!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    return false;
+                }
+            }
+
+            if (NewExpenseActivity.method == SplitMethod.PERCENT && total != 100) {
+                Snackbar.make(getWindow().getDecorView().getRootView()
+                                , "Oops! The percents don't add up to 100%. ", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return false;
+            }
+
+            if (NewExpenseActivity.method == SplitMethod.EQUAL && total != Double.parseDouble(amount)) {
+                Snackbar.make(getWindow().getDecorView().getRootView()
+                                , "Oops! The amounts don't add up to " + amount, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return false;
+            }
+
+            double splitAmount = Double.parseDouble(amount);
+            int numParticipants = participantsToSplit.size() ;
+
+
+            for (int i = 0; i < participantsToSplit.size(); i++) {
+                User participant = participantsToSplit.get(i);
+                Double input = inputs.get(i);
+
+                if (NewExpenseActivity.method == SplitMethod.EQUAL) {
+                    NewExpenseActivity.result.put(participant, splitAmount / numParticipants);
+                } else if (NewExpenseActivity.method == SplitMethod.PERCENT) {
+                    NewExpenseActivity.result.put(participant, splitAmount * input / 100);
+                } else if (NewExpenseActivity.method == SplitMethod.EXACT) {
+                    NewExpenseActivity.result.put(participant, input);
+                }
+            }
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
