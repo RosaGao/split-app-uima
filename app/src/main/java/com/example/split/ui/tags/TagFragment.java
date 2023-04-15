2dc67c3edddc26461e89f033b55175406687518f
franksun <franksun1027@gmail.com>
2023-04-15 17:49:27 -0400

    tag now working with firebase

--- app/src/main/java/com/example/split/ui/tags/TagFragment.java
+++ app/src/main/java/com/example/split/ui/tags/TagFragment.java
@@ -2,39 +2,65 @@ package com.example.split.ui.tags;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.split.R;
 import com.example.split.Tag.TagsAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class TagFragment extends Fragment {
 
     private DatabaseReference mDatabaseReference;
 
-
     private RecyclerView tagsRecyclerView;
     private TagsAdapter tagsAdapter;
-    private List<String> tagNames = new ArrayList<>(Arrays.asList("food", "travel", "groceries", "utilities", "business"));
+    private List<String> tagNames = new ArrayList<>();
 
     public TagFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize the DatabaseReference
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("tags");
+        mDatabaseReference.addValueEventListener(tagsValueEventListener);
+
+        // Add the default tags to the database
+        List<String> defaultTags = Arrays.asList("food", "travel", "groceries", "utilities", "business");
+
+        for (String tag : defaultTags) {
+            saveTagToDatabase(tag);
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mDatabaseReference != null && tagsValueEventListener != null) {
+            mDatabaseReference.removeEventListener(tagsValueEventListener);
+        }
     }
 
     @Override
@@ -68,8 +94,7 @@ public class TagFragment extends Fragment {
                             public void onClick(DialogInterface dialog, int which) {
                                 String tagName = tagNameInput.getText().toString();
                                 if (!tagName.isEmpty()) {
-                                    tagNames.add(tagName);
-                                    tagsAdapter.notifyDataSetChanged();
+                                    saveTagToDatabase(tagName);
                                 }
                             }
                         })
@@ -85,4 +110,27 @@ public class TagFragment extends Fragment {
             }
         });
     }
+
+    private ValueEventListener tagsValueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            tagNames.clear();
+            for (DataSnapshot tagSnapshot : dataSnapshot.getChildren()) {
+                String tagName = tagSnapshot.getValue(String.class);
+                tagNames.add(tagName);
+            }
+            tagsAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.e("TagFragment", "Error fetching tags: ", databaseError.toException());
+        }
+    };
+
+    private void saveTagToDatabase(String tagName) {
+        String tagId = mDatabaseReference.push().getKey();
+        mDatabaseReference.child(tagId).setValue(tagName);
+    }
 }
+
