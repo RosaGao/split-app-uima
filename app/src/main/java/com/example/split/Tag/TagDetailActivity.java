0420db9399eee7a4556d874ecb47ae7c53f13c72
kyang117 <kyang11702@gmail.com>
2023-04-15 20:09:36 -0400

    working on menu for tag detail and also created activity for tag detail page

---
+++ app/src/main/java/com/example/split/Tag/TagDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.split.Tag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.split.R;
+
+public class TagDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag_detail);
+    }
+}
