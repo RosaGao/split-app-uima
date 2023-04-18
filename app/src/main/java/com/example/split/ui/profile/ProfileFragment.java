1d45f3644faf70db969e3d700396e2f939909d5c
franksun <franksun1027@gmail.com>
2023-04-17 20:01:43 -0400

    currently the profile page works and extract information from the database

--- app/src/main/java/com/example/split/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/split/ui/profile/ProfileFragment.java
@@ -4,18 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
 import com.example.split.R;
-import com.example.split.ui.profile.ProfileViewModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
+
+    private EditText nameField;
+    private EditText phoneField;
+    private EditText emailField;
+    private EditText passwordField;
+
+    private DatabaseReference databaseReference;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -33,14 +47,38 @@ public class ProfileFragment extends Fragment {
         // Initialize the ViewModel
         profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        // Get the TextView from the layout
-        TextView textView = view.findViewById(R.id.text_chat);
+        // Get references to all EditText fields
+        nameField = view.findViewById(R.id.name_field);
+        phoneField = view.findViewById(R.id.phone_field);
+        emailField = view.findViewById(R.id.email_field);
+        passwordField = view.findViewById(R.id.password_field);
 
-        // Observe the LiveData from the ViewModel and update the TextView
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        // Get current user's ID
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        // Get reference to the current user's data in the Firebase Realtime Database
+        databaseReference = FirebaseDatabase.getInstance().getReference("users").child(userId);
+
+        // Listen for changes to the user's data in the database
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Get user's data
+                String name = dataSnapshot.child("name").getValue(String.class);
+                String phone = dataSnapshot.child("phone").getValue(String.class);
+                String email = dataSnapshot.child("email").getValue(String.class);
+                String password = dataSnapshot.child("password").getValue(String.class);
+
+                // Populate EditText fields with user's data
+                nameField.setText(name);
+                phoneField.setText(phone);
+                emailField.setText(email);
+                passwordField.setText(password);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error
             }
         });
     }
