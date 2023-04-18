d2ace93f91fda2c69cb70d052a95fa9c1fc3e849
franksun <franksun1027@gmail.com>
2023-04-17 20:56:06 -0400

    Profile page functional todo: improve the layouts

--- app/src/main/java/com/example/split/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/split/ui/profile/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -28,6 +29,7 @@ public class ProfileFragment extends Fragment {
     private EditText phoneField;
     private EditText emailField;
     private EditText passwordField;
+    private Button saveButton;
 
     private DatabaseReference databaseReference;
 
@@ -47,11 +49,12 @@ public class ProfileFragment extends Fragment {
         // Initialize the ViewModel
         profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        // Get references to all EditText fields
+        // Get references to all EditText fields and the Save button
         nameField = view.findViewById(R.id.name_field);
         phoneField = view.findViewById(R.id.phone_field);
         emailField = view.findViewById(R.id.email_field);
         passwordField = view.findViewById(R.id.password_field);
+        saveButton = view.findViewById(R.id.save_button);
 
         // Get current user's ID
         String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -79,6 +82,23 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // Handle error
+            }
+        });
+
+        // Set up Save button click listener
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Update user's data in the database
+                String name = nameField.getText().toString().trim();
+                String phone = phoneField.getText().toString().trim();
+                String email = emailField.getText().toString().trim();
+                String password = passwordField.getText().toString().trim();
+
+                databaseReference.child("name").setValue(name);
+                databaseReference.child("phone").setValue(phone);
+                databaseReference.child("email").setValue(email);
+                databaseReference.child("password").setValue(password);
             }
         });
     }
