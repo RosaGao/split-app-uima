5b31b33460381c88291e2390b3237a79c91ce8e1
kyang117 <kyang11702@gmail.com>
2023-04-19 07:28:31 -0400

    created adapter for recycler view specific to tag detail page, using tag_expense_layout and tag_expense_list

---
+++ app/src/main/java/com/example/split/Tag/TagExpenseListRecyclerViewAdapter.java
@@ -0,0 +1,75 @@
+package com.example.split.Tag;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.split.R;
+import com.example.split.entity.Expense;
+import com.example.split.expenseList.ExpenseDetailActivity;
+import com.example.split.Tag.TagExpenseViewHolder;
+import com.example.split.ui.home.HomeFragment;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TagExpenseListRecyclerViewAdapter
+        extends RecyclerView.Adapter<TagExpenseViewHolder> {
+
+    private TagDetailActivity myParentActivity;
+    private List<Expense> myExpenses = new ArrayList<>();
+    public TagExpenseListRecyclerViewAdapter(TagDetailActivity parent, List<Expense> allExpenses, boolean twoPane) {
+        myExpenses = allExpenses;
+        myParentActivity = parent;
+    }
+
+    @NonNull
+    @Override
+    public TagExpenseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.tag_expense_layout, parent, false);
+        return new TagExpenseViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TagExpenseViewHolder holder, int position) {
+
+        holder.description.setText(myExpenses.get(position).getDescription());
+        holder.date.setText(myExpenses.get(position).getDate());
+
+        Expense expense = myExpenses.get(position);
+        double borrowing = expense.getBorrowing();
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        if (borrowing > 0) {
+            holder.status.setText("You borrowed $" + df.format(borrowing));
+            holder.status.setTextColor(holder.status.getResources().getColor(R.color.red));
+        } else {
+            holder.status.setText("You are owed $" + df.format(Math.abs(borrowing)));
+            holder.status.setTextColor(holder.status.getResources().getColor(R.color.green));
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                Intent intent = new Intent(context, TagDetailActivity.class);
+                intent.putExtra("expense_id", myExpenses.get(position).getExpenseId());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return myExpenses.size();
+    }
+
+}
