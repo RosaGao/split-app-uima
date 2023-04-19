060081b5cb70b0fedf97121d40281990e5ab846d
Brandon Tang <btang13@jh.edu>
2023-04-18 22:11:00 -0400

    expense detail recycler view

--- app/src/main/java/com/example/split/expenseList/ExpenseDetailRecyclerViewAdapter.java
+++ app/src/main/java/com/example/split/expenseList/ExpenseDetailRecyclerViewAdapter.java
@@ -1,4 +1,16 @@
 package com.example.split.expenseList;
 
-public class ExpenseDetailRecyclerViewAdapter {
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.split.entity.User;
+
+import java.util.List;
+
+public class ExpenseDetailRecyclerViewAdapter extends RecyclerView.Adapter<ExpenseDetailViewHolder> {
+
 }
---
+++ app/src/main/java/com/example/split/expenseList/ExpenseDetailViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.split.expenseList;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ExpenseDetailViewHolder extends RecyclerView.ViewHolder{
+    public ExpenseDetailViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
